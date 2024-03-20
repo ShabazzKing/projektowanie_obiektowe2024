fcb681c249fc1517f6e81ceb32858f7f6f6bf027
Łukasz Konieczny <ftefrjbhfvasf32@o2.pl>
2024-03-20 21:16:20 +0100

    Task 1: Sorting numbers implemented

--- zadanie_1/random_numbers.pas
+++ zadanie_1/random_numbers.pas
@@ -19,9 +19,31 @@ begin
         randomNumbers[i] := randomRange(inf, sup + 1);
 end;
 
+procedure sortNumbers();
+var
+    swapped : boolean;
+    temp : integer;
+    n, i : integer;
+begin
+    n := nOfNumbers;
+    repeat
+        swapped := false;
+        for i := 2 to n do
+            if randomNumbers[i - 1] > randomNumbers[i] then
+            begin
+                temp := randomNumbers[i - 1];
+                randomNumbers[i - 1] := randomNumbers[i];
+                randomNumbers[i] := temp;
+                swapped := true;
+            end;
+        n := n - 1;
+    until not swapped;
+end;
+
 begin
     randomize();
     getNumbers();
+    sortNumbers();
     for i := 1 to nOfNumbers do
         write(randomNumbers[i], '  ');
     writeln();
